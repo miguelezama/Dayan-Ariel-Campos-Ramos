e69124756220584952f70edf3085523f3acea112
dayanariel <dayan.campos15@est.unanleon.edu.ni>
2019-07-07 18:53:26 -0600

    version 2.4

--- app/src/main/java/com/example/weather/UI/DailyWeatherActivity.kt
+++ app/src/main/java/com/example/weather/UI/DailyWeatherActivity.kt
@@ -2,6 +2,8 @@ package com.example.weather.UI
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.weather.Models.Day
 import com.example.weather.R
 
 class DailyWeatherActivity : AppCompatActivity() {
@@ -9,6 +11,16 @@ class DailyWeatherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_weather)
+
+        intent.let {
+
+            //rellenar nuestra lista
+
+            val days:ArrayList<Day> = it.getParcelableArrayListExtra(MainActivity.DAYLY_WEATHER)
+
+            Toast.makeText(this, days.get(0).getFormattedTime(), Toast.LENGTH_LONG).show()
+        }
+
 
     }
 }
