909368c429f4521413f751b9546ba4311ee735e5
dayanariel <dayan.campos15@est.unanleon.edu.ni>
2019-07-07 23:54:31 -0600

    version 3.8

--- app/src/main/java/com/example/weather/UI/HourlyWeatherActivity.kt
+++ app/src/main/java/com/example/weather/UI/HourlyWeatherActivity.kt
@@ -1,14 +1,30 @@
 package com.example.weather.UI
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import com.example.weather.Adapters.HourAdapter
+import com.example.weather.Models.Hour
 import com.example.weather.R
+import kotlinx.android.synthetic.main.activity_hourly_weather.*
 
 class HourlyWeatherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hourly_weather)
+
+        hourlyRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        intent.let{
+
+            val hours:ArrayList<Hour> = it.getParcelableArrayListExtra(MainActivity.HOURlY_WEATHER)
+
+            hourlyRecyclerView.adapter = HourAdapter(this, hours)
+
+
+        }
 
 
     }
