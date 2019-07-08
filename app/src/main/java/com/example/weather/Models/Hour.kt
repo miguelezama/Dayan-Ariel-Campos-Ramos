09a5acf7e303e64c7e72d367a33a0930d637ce47
dayanariel <dayan.campos15@est.unanleon.edu.ni>
2019-07-07 23:05:46 -0600

    version 3.4

--- app/src/main/java/com/example/weather/Models/Hour.kt
+++ app/src/main/java/com/example/weather/Models/Hour.kt
@@ -1,9 +1,39 @@
 package com.example.weather.Models
 
+import android.os.Parcel
+import android.os.Parcelable
 import java.text.SimpleDateFormat
 import java.util.*
 
-class Hour(val time: Long, val temp:Double, val precip:Double) {
+class Hour(val time: Long, val temp:Double, val precip:Double): Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readLong(),
+        parcel.readDouble(),
+        parcel.readDouble()
+    )
+
+
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeLong(time)
+        parcel.writeDouble(temp)
+        parcel.writeDouble(precip)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Hour> {
+        override fun createFromParcel(parcel: Parcel): Hour {
+            return Hour(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Hour?> {
+            return arrayOfNulls(size)
+        }
+    }
 
     fun getFormattedTime():String{
 
@@ -15,11 +45,6 @@ class Hour(val time: Long, val temp:Double, val precip:Double) {
 
         return dayOfTheWeek
     }
-
-
-
-
-
 }
 
 
